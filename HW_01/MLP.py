19aa5fff3967c538382d83e18d2190b0c79a01cb
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2023-11-05 18:59:00 +0100

    loads the dataset and creates a tuple

--- HW_01/MLP.py
+++ HW_01/MLP.py
@@ -3,5 +3,12 @@ from sklearn.datasets import load_digits
 # n_class: the digits from 0 to 9
 # return_X_y = False: (data, target) as a tuple
 # as_frame = False: returns (data, target) as a numpy array
-digits_dataset = load_digits(n_class=10, return_X_y=False, as_frame=False)
-print(digits_dataset)
+digits_dataset = load_digits(n_class = 10, return_X_y = False, as_frame = False)
+
+# Accessing the data (input) and target (output)
+data = digits_dataset.data
+target = digits_dataset.target
+
+# Iterating over the dataset and creating a tuple of input and output
+input_output_tuple = [(data[i], target[i]) for i in range (len(data))]
+print(input_output_tuple[0])
