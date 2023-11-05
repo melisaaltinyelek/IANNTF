8d645cd817da617bfe3c4cc8491a319511320e1a
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2023-11-05 18:15:51 +0100

    deleted the old hw

--- hw_1/kittyconcert.py
+++
@@ -1,8 +0,0 @@
-from cat import Cat
-
-# Creating instances/objects from the Cat class.
-cat1 = Cat("Kittosaurus Rex")
-cat2 = Cat("Snowball IX")
-
-# Calling greet function on cat1 instance and passing cat2 instance so that it is greeted by cat1.
-cat1.greet(cat2)
